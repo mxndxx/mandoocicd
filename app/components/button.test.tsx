c63a4bf816eb8c432554c5f91f6705c721e31b77
mandoo <mandoo@Jmandooui-MacBookAir.local>
2025-02-03 15:30:01 +0900

    update

--- app/components/button.test.tsx
+++
@@ -1,9 +0,0 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
-import Button from "./Button";
-
-test("Button component renders with correct label", () => {
-  render(<Button label="Click me" />);
-  const buttonElement = screen.getByText(/Click me/i);
-  expect(buttonElement).toBeInTheDocument();
-});
