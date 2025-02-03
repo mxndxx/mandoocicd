20bccba5dd16a636c19f0397267acdb954872473
mandoo <mandoo@Jmandooui-MacBookAir.local>
2025-02-03 14:56:18 +0900

    updatae layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <title>title</title>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
